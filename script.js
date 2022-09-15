de2ab4e1374b3ecba3ac0b5dc13bfb5c4302cc4d
IttsCuffa <riosfacundo.isaias@gmail.com>
2022-09-15 17:18:38 -0300

    Header responsive terminado

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const d=document;
-const $btnMenu=d.querySelector(".btn-menu");
-const $nav=d.querySelector(".nav")
-$btnMenu.addEventListener("click",e=>{
-    $nav.classList.toggle("none")
-})
+/* Menu mobile clousure */
+(()=>{
+    const d=document;
+    const $btnMenu=d.querySelector(".btn-menu");
+    const $nav=d.querySelector(".nav-menu");
+
+    $btnMenu.addEventListener("click",e=>{
+        $btnMenu.firstElementChild.classList.toggle("none")
+        $btnMenu.lastElementChild.classList.toggle("none")
+        $nav.classList.toggle("close-menu");
+        
+    })
+    /* Este evento aparecera cuando apretemos en unos de los enlaces validos */
+    d.addEventListener("click",(e)=>{
+        if(!e.target.matches(".nav-menu a")) return false;
+        $btnMenu.firstElementChild.classList.remove("none")
+        $btnMenu.lastElementChild.classList.add("none")
+        $nav.classList.remove("close-menu");
+    })
+})();
