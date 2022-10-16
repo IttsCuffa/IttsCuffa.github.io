26caf1363758301e2e619d354a2cb6c7ccbefe25
IttsCuffa <riosfacundo.isaias@gmail.com>
2022-10-16 13:32:20 -0300

    formulario anda

--- script.js
+++ script.js
@@ -21,7 +21,10 @@
 
 })();
 /* Dark-mode & Light-mode */
-(()=>{
+
+
+/* El funcionamiento es correcto pero la logica esta mal. */
+/* (()=>{
     const d=document;
    const $btnMode=d.querySelector(".btn-mode");
    const body=d.querySelector("body");
@@ -30,19 +33,111 @@
    const header=d.querySelector("header")
    const $svgLinks=d.querySelectorAll(".svg-link")
     $btnMode.addEventListener("click",e=>{
-        $btnMode.firstElementChild.classList.add("none");
-        $btnMode.lastElementChild.classList.remove("none");
+        $btnMode.firstElementChild.classList.toggle("none");
+        $btnMode.lastElementChild.classList.toggle("none");
         body.classList.toggle("dark-mode");
        header.classList.toggle("dark-mode");
         $navMenu.classList.toggle("dark-mode");
-        /* Nav items  */
+      
         $menuLinks.forEach(el => {
             el.classList.toggle("dark-mode")
           });
        $svgLinks.forEach(el=>{
         el.classList.toggle("dark-mode")
        })
-       
+     
     })
+    })(); */
 
+
+    (()=>{
+        const d=document;
+        const $btnMode=d.querySelector(".btn-mode");
+   const body=d.querySelector("body");
+    const $menuLinks=d.querySelectorAll(".menu-link ")
+   const $navMenu=d.querySelector(".nav-menu")
+   const header=d.querySelector("header")
+   const $svgLinks=d.querySelectorAll(".svg-link")
+        d.addEventListener("click",e=>{
+           /*  if(e.target.matches(".btn-mode svg")) {
+                if($btnMode.classList)
+            } */
+            
+           
+        })
+
+        let darkMode=()=>{}
+     
+        body.classList.add("dark-mode");
+       header.classList.add("dark-mode");
+        $navMenu.classList.add("dark-mode");
+      
+        $menuLinks.forEach(el => {
+            el.classList.add("dark-mode")
+          });
+       $svgLinks.forEach(el=>{
+        el.classList.add("dark-mode")
+       })
+        let lightMode=()=>{}
+       
+        body.classList.remove("dark-mode");
+       header.classList.remove("dark-mode");
+        $navMenu.classList.remove("dark-mode");
+      
+        $menuLinks.forEach(el => {
+            el.classList.remove("dark-mode")
+          });
+       $svgLinks.forEach(el=>{
+        el.classList.remove("dark-mode")
+       })
+        
+    })();
+
+
+
+
+
+  /*  (()=>{ */
+    /*   /* localStorage */
+     /*  saveLocalStorage(body.classList.value); */
+      /*  console.log(body.classList.value) 
+ 
+      let saveLocalStorage=(mode) =>{
+        localStorage.setItem( "theme",mode);
+    }
+
+    let getLocalStorage=()=>{
+        localStorage.getItem("theme")
+    }
+   })();
+*/
+/* Contacto form */
+(()=>{
+    const d=document;
+    const $form=d.querySelector(".form")
+    const $response=d.querySelector(".contact-form-response")
+    $form.addEventListener("submit",e=>{
+        e.preventDefault();
+        fetch("https://formsubmit.co/ajax/riosfacundo.isaias@gmail.com",{
+            method:"POST",
+            body: new FormData(e.target),
+        }).then((res)=>(res.ok ? res.json():Promise.reject(res)))
+        .then((json)=>{
+    /* ventana modal y loader faltan */
+    location.hash="#gracias";
+    $form.reset()
+
+        })
+           
+        .catch((err)=>{
+            let message=err.statusText || "Ocurrio un error al enviar el formulario,intente nuevamente";
+            $response.querySelector("h3").innerHTML=`Error${err.status}:${message}`;
+
+        }).finally(()=>{
+            setTimeout(()=>{
+                location.hash="#close";
+            },3000)
+        })
+    })
 })();
+/*  */
